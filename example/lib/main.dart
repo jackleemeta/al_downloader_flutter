63c03e2131118e21fbf212decfc1b343dd477e49
jackleemeta <jackleemeta@outlook.com>
2022-04-08 14:17:54 +0800

    optimize demo - more test file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -277,7 +277,14 @@ final kTestVideos = [
   "http://vfx.mtime.cn/Video/2019/03/21/mp4/190321153853126488.mp4",
   "http://vfx.mtime.cn/Video/2019/03/19/mp4/190319222227698228.mp4",
   "http://vfx.mtime.cn/Video/2019/03/19/mp4/190319212559089721.mp4",
-  "http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4"
+  "http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/09/mp4/190309153658147087.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/12/mp4/190312083533415853.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/12/mp4/190312143927981075.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/13/mp4/190313094901111138.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/14/mp4/190314102306987969.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/14/mp4/190314223540373995.mp4",
+  "http://vfx.mtime.cn/Video/2019/03/19/mp4/190319125415785691.mp4"
 ];
 
 final kTestOthers = ["https://www.orimi.com/pdf-test.pdf"];
