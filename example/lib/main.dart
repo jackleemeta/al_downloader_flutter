61e702f754acb929b6f63bc10ba7e7811dacf068
jackleemeta <jackleemeta@outlook.com>
2023-09-12 17:28:08 +0800

    chore: update test data

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -302,16 +302,9 @@ extension _ALDownloaderStatusExtension on ALDownloaderStatus {
 final models = kTestVideos.map((e) => DownloadModel(e)).toList();
 
 final kTestVideos = [
-  'http://vfx.mtime.cn/Video/2019/03/19/mp4/190319222227698228.mp4',
-  // 'http://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4', failed
-  'http://vfx.mtime.cn/Video/2019/03/21/mp4/190321153853126488.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/19/mp4/190319212559089721.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/18/mp4/190318231014076505.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/09/mp4/190309153658147087.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/12/mp4/190312083533415853.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/12/mp4/190312143927981075.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/13/mp4/190313094901111138.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/14/mp4/190314102306987969.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/14/mp4/190314223540373995.mp4',
-  'http://vfx.mtime.cn/Video/2019/03/19/mp4/190319125415785691.mp4'
+  'https://media.w3.org/2010/05/sintel/trailer.mp4',
+  'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+  'http://vjs.zencdn.net/v/oceans.mp4',
+  'http://mirror.aarnet.edu.au/pub/TED-talks/911Mothers_2010W-480p.mp4',
+  'http://downsc.chinaz.net/Files/DownLoad/sound1/201906/11582.mp3'
 ];
